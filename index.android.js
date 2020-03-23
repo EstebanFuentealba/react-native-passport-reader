d9119e110f4191dea0fcd1eb7f4954ba36124daa
Esteban Fuentealba <efuentealba@json.cl>
2020-03-23 20:36:01 -0300

    Fixed: NativeEventEmitter

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
-import { NativeModules } from "react-native";
+import { NativeModules, NativeEventEmitter } from "react-native";
 
 const { RNPassportReader } = NativeModules;
 const Emitter = new NativeEventEmitter(RNPassportReader);
